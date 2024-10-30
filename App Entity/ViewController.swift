aa56598ab898011746412e8e1592451753ce2791
wahson <wahson@appest.com>
2024-10-30 20:09:28 +0800

    Initial Commit

---
+++ App Entity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Entity
+//
+//  Created by wahson on 2024/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
